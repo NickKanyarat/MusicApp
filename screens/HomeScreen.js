52e0b384c8d626a3cfb12cc5afc09fdeb71b73d7
NickKanyarat <kantarat.yu@kkumail.com>
2024-02-27 19:55:10 +0700

    Top 30 tracks

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,79 +1,67 @@
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-  ActivityIndicator,
-  Image,
-} from "react-native";
 import React, { useState, useEffect } from "react";
+import { SafeAreaView, StyleSheet, Text, View, ScrollView, Image, ActivityIndicator } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const HomeScreen = () => {
+  const [accessToken, setAccessToken] = useState(null);
   const [loading, setLoading] = useState(true);
   const [topTracks, setTopTracks] = useState([]);
 
   useEffect(() => {
-    const fetchTopTracks = async () => {
+    const fetchData = async () => {
       try {
-        const response = await fetch(
-          "https://api.spotify.com/v1/playlists/3cEYpjA9oz9GiPac4AsH4n/tracks", // Spotify top tracks playlist ID
-          {
+        // Get access token from AsyncStorage
+        const token = await AsyncStorage.getItem("accessToken");
+        if (token) {
+          setAccessToken(token);
+
+          // Fetch top tracks
+          const response = await fetch("https://api.spotify.com/v1/me/top/tracks?limit=30", {
             method: "GET",
             headers: {
-              Authorization:
-                "Bearer BQCVZ5c9m_1vaY3E7yiyV9fOMagaY9dZf9fgarpHnI_y2LfVx1N9n4FBteNCHts2PlFP3OTEgepmRYz0OQvUXbbQGSkH2drU_ATGk8aAWWRRtk1ObzrJg51wSbUlIN-6ulMh3ce_PdpTBQdftwdDyORZJSr6B3atvALy8aUGGqwH7nuAjFSmujYDXzlsi5pYwjsegx-P6Jb-Mv_Ik5yrE7LMXyZSYCMu6Psws3pSA9Kp3aTLMGyA", // Replace with your Spotify access token
+              Authorization: `Bearer ${token}`,
               "Content-Type": "application/json",
             },
-          }
-        );
+          });
 
-        const data = await response.json();
-        const tracks = data.items.map((item) => item.track); // Extracting the tracks from the response
-        setTopTracks(tracks);
-        setLoading(false);
+          if (!response.ok) {
+            throw new Error("Failed to fetch top tracks");
+          }
+
+          const data = await response.json();
+          setTopTracks(data.items);
+          setLoading(false);
+        }
       } catch (error) {
-        console.error("Error fetching top tracks:", error);
+        console.error("Error fetching data:", error);
         setLoading(false);
       }
     };
 
-    fetchTopTracks();
+    fetchData();
   }, []);
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.screen}>
-        <Text style={{ color: "white", fontSize: 30, fontWeight: "bold" }}>
-          Top Tracks
-        </Text>
+        <Text style={styles.title}>Top Tracks</Text>
         {loading ? (
           <ActivityIndicator size="large" color="#ffffff" />
         ) : (
-          <View>
+          <ScrollView contentContainerStyle={styles.tracksContainer}>
             {topTracks.map((track, index) => (
-              <View
-                key={index}
-                style={{
-                  flexDirection: "row",
-                  alignItems: "center",
-                  marginBottom: 10,
-                }}
-              >
+              <View key={index} style={styles.trackItem}>
                 <Image
                   source={{ uri: track.album.images[0].url }}
-                  style={{ width: 50, height: 50, marginRight: 10 }}
+                  style={styles.trackImage}
                 />
-                <View>
-                  <Text style={{ color: "white", fontSize: 18 }}>
-                    {index + 1}. {track.name}
-                  </Text>
-                  <Text style={{ color: "white", fontSize: 14 }}>
-                    {track.artists.map((artist) => artist.name).join(", ")}
-                  </Text>
+                <View style={styles.trackInfo}>
+                  <Text style={styles.trackName}>{track.name}</Text>
+                  <Text style={styles.trackArtists}>{track.artists.map(artist => artist.name).join(", ")}</Text>
                 </View>
               </View>
             ))}
-          </View>
+          </ScrollView>
         )}
       </View>
     </SafeAreaView>
@@ -83,12 +71,41 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 20,
     backgroundColor: "black",
   },
   screen: {
-    marginHorizontal: 20,
-    marginVertical: 20,
+    flex: 1,
+    padding: 20,
+  },
+  title: {
+    color: "white",
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  tracksContainer: {
+    alignItems: "center",
+  },
+  trackItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  trackImage: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+  },
+  trackInfo: {
+    flex: 1,
+  },
+  trackName: {
+    color: "white",
+    fontSize: 18,
+  },
+  trackArtists: {
+    color: "lightgray",
+    fontSize: 14,
   },
 });
 
