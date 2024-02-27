ad979985a1d3414207b6d58391715add17416af7
NickKanyarat <kantarat.yu@kkumail.com>
2024-02-27 19:56:22 +0700

    User Profile Data

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,77 +1,81 @@
+import React, { useState, useEffect } from "react";
 import {
   SafeAreaView,
   StyleSheet,
   Text,
   View,
-  ActivityIndicator,
   Image,
+  ActivityIndicator,
 } from "react-native";
-import React, { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const ProfileScreen = () => {
-  const [loading, setLoading] = useState(true);
   const [userData, setUserData] = useState(null);
-  const [profileImage, setProfileImage] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchUserProfile = async () => {
+    const fetchData = async () => {
       try {
-        // Replace 'YOUR_ACCESS_TOKEN' with the access token obtained after authentication
-        const response = await fetch("https://api.spotify.com/v1/me", {
-          method: "GET",
-          headers: {
-            Authorization:
-              "Bearer BQDBoOMic_uStABngF0rBmKqZgNgbJsGQVDMzfChG0p1eeEX3UXqNZNB56JtEI8I49PZgx4rNkeFj5jX60O79D5VYY4nqnCjgrCrwy79LAee2Js0OUZNPzjDjaAfnBzb7a5vqVJr6CwLn_rZLkWxOIsg6zRMakeKv2UzIfsKW4K-EF0-Uc7Vk3mqTzrs-jEyakRLWMlIVXeDpxCoXbZM2H3w1awVOpnwdtFqqSqsKoUxUoA2vEe_",
-            "Content-Type": "application/json",
-          },
-        });
+        // Get access token from AsyncStorage
+        const token = await AsyncStorage.getItem("accessToken");
+        if (token) {
+          // Fetch user data
+          const response = await fetch("https://api.spotify.com/v1/me", {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+          });
 
-        const data = await response.json();
-        setUserData(data);
-        setLoading(false);
+          if (!response.ok) {
+            throw new Error("Failed to fetch user data");
+          }
 
-        // Fetch profile image
-        const imageUrl = data.images.length > 0 ? data.images[0].url : null;
-        setProfileImage(imageUrl);
+          const data = await response.json();
+          setUserData(data);
+          setLoading(false);
+        }
       } catch (error) {
-        console.error("Error fetching user profile:", error);
+        console.error("Error fetching user data:", error);
         setLoading(false);
       }
     };
 
-    fetchUserProfile();
+    fetchData();
   }, []);
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.screen}>
-        <Text style={{ color: "white", fontSize: 30, fontWeight: "bold" }}>
-          User Profile
-        </Text>
+        <Text style={styles.title}>User Profile</Text>
         {loading ? (
           <ActivityIndicator size="large" color="#ffffff" />
         ) : (
-          <View>
-            <Text style={{ color: "white", fontSize: 18 }}>
-              Display Name: {userData.display_name}
-            </Text>
-            <Text style={{ color: "white", fontSize: 18 }}>
-              Email: {userData.email}
-            </Text>
-            <Text style={{ color: "white", fontSize: 18 }}>
-              Country: {userData.country}
-            </Text>
-            {profileImage && (
+          <View style={styles.profileInfo}>
+            <View style={styles.labelContainer}>
               <Image
-                style={{
-                  width: 100,
-                  height: 100,
-                  borderRadius: 50,
-                  marginTop: 10,
+                source={{
+                  uri:
+                    userData.images.length > 0 ? userData.images[0].url : null,
                 }}
-                source={{ uri: profileImage }}
+                style={styles.profileImage}
               />
-            )}
+            </View>
+            <View style={styles.labelContainer}>
+              <Text style={styles.label}>Name:</Text>
+              <Text style={styles.value}>{userData.display_name}</Text>
+            </View>
+
+            <View style={styles.labelContainer}>
+              <Text style={styles.label}>Email:</Text>
+              <Text style={styles.value}>{userData.email}</Text>
+            </View>
+
+            <View style={styles.labelContainer}>
+              <Text style={styles.label}>Country:</Text>
+              <Text style={styles.value}>{userData.country}</Text>
+            </View>
           </View>
         )}
       </View>
@@ -82,12 +86,40 @@ const ProfileScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 20,
     backgroundColor: "black",
   },
   screen: {
-    marginHorizontal: 20,
-    marginVertical: 20,
+    flex: 1,
+    padding: 20,
+  },
+  title: {
+    color: "white",
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  profileInfo: {
+    alignItems: "center",
+  },
+  labelContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  label: {
+    color: "lightgray",
+    fontSize: 18,
+    marginRight: 5,
+  },
+  value: {
+    color: "white",
+    fontSize: 18,
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginTop: 10,
   },
 });
 
