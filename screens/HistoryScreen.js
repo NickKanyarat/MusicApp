8d8b2b619fd96b653c7e18d179864857583dee98
NickKanyarat <kantarat.yu@kkumail.com>
2024-03-01 03:32:25 +0700

    Add title.

--- screens/HistoryScreen.js
+++ screens/HistoryScreen.js
@@ -1,14 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const HistoryScreen = () => {
   return (
-    <View>
-      <Text>HistoryScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={styles.container}>
+      <View style={styles.screen}>
+        <Text style={styles.title}>History</Text>
+        <View style={styles.content}>
+            <Text style={{ color: "white" }}>Record</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default HistoryScreen
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
-const styles = StyleSheet.create({})
+export default HistoryScreen;
